9bb8bfdeb34b225656eb9594fef2330ed2d65bd9
markogrady1 <markogrady.code@gmail.com>
2014-07-01 21:09:06 +0100

    add non violent theme

--- java/strategyPattern/Bazooka.java
+++ java/strategyPattern/Bazooka.java
@@ -3,6 +3,6 @@ package strategyPattern;
 public class Bazooka implements Weapon {
 	public void action() {
 		System.out
-				.println("BadooooMAAAAAAAAA you gone man!!!!!!!!!!!!!!!!!!!!!!!!");
+				.println("BAAAZZOOOOMAAAAAAAAA!!!!!!!!!!!!!!!!!!!!!!!!");
 	}
 }
