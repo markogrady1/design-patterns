0da32bd0b2399cc2d877b40d0677dc592c6e0e16
markogrady1 <markogrady.code@gmail.com>
2014-07-02 19:06:14 +0100

    Add comments

--- php/observer_pattern/Administrator.php
+++ php/observer_pattern/Administrator.php
@@ -1,6 +1,15 @@
 <?php 
 include_once('User.php');
-
+/*
+|===========================================================
+|
+|-----------------------------------------------------------
+|	The Administrator is the 'Subject' in this pattern
+|-----------------------------------------------------------
+|
+|===========================================================
+|
+*/
 interface Administrator{
 	public function registerUser(User $user);
 	public function removeUser(User $user);
