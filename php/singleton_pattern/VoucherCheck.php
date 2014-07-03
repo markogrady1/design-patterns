88d7cbab62f444216a253e45b33787f54a1d1073
grady <markogrady18@gmail.com>
2014-07-03 23:11:07 +0100

    add singleton pattern to php

---
+++ php/singleton_pattern/VoucherCheck.php
@@ -0,0 +1,12 @@
+<?php 
+include 'VoucherProvider.php';
+
+class VoucherCheck{
+
+	public function __construct(){}
+
+	public function signIn(){
+		$newVoucher = VoucherProvider::getVoucher();
+	}
+
+}
---
+++ php/singleton_pattern/VoucherProvider.php
@@ -0,0 +1,17 @@
+<?php 
+
+class VoucherProvider{
+	private static $instance = null;
+	public static function getVoucher(){
+		if(self::$instance == null){
+			self::$instance = new VoucherProvider();
+			self::$instance->setDateStamp();
+			return 'instance created';
+		}
+		return self::$instance;
+	}
+
+	public function setDateStamp(){
+		// start date stamp
+	}
+}
