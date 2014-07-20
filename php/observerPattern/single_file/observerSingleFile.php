2b9181e4171b9c648be747785de9944f7ca727b9
markogrady1 <markogrady.code@gmail.com>
2014-07-20 22:06:58 +0100

    Update observerSingleFile.php

--- php/observerPattern/single_file/observerSingleFile.php
+++ php/observerPattern/single_file/observerSingleFile.php
@@ -117,12 +117,10 @@ class MagazineUser implements User{
 |
 */
 $adminDesk = new AdminDesk();
-
 $magUser = new MagazineUser();
 $magUser->userReg($adminDesk);
 $appUser = new ApplicationUser();
 $appUser->userReg($adminDesk);
-
 $adminDesk->setConferenceDetails('London',1000,'Prince Albert Hall');
 $adminDesk->setConferenceDetails('New York',20000,'New Jersey Hall');
 $adminDesk->displayDetails();
