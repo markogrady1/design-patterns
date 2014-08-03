6bc2c356e7357d7173bf770a42f9b1d5777869dc
markogrady1 <markogrady.code@gmail.com>
2014-08-03 19:42:11 +0100

    Tidy code

--- php/decoratorPattern/decoratorUse.php
+++ php/decoratorPattern/decoratorUse.php
@@ -3,11 +3,17 @@
 include('../../autoload.php');
 
 $laptop = new decoratorPattern\Laptop;
+
 $money = $laptop->cost();
-//echo $laptop->getDescription().'  '.$laptop->cost();
+
 $keyboard = new decoratorPattern\Keyboard($laptop);
+
 echo $keyboard->getDescription();
+
 echo $laptop->cost();
+
 $mouse = new decoratorPattern\Mouse($laptop);
+
 echo $mouse->getDescription();
+
 echo $mouse->cost();
