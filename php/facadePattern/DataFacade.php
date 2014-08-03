d46eb34fec14294995279454372d1f41c25524f7
markogrady1 <markogrady.code@gmail.com>
2014-08-03 19:45:53 +0100

    Clean code

--- php/facadePattern/DataFacade.php
+++ php/facadePattern/DataFacade.php
@@ -5,36 +5,40 @@ class DataFacade {
 private $passengerNo;
 
 	public function retrieveStats($passID){
-	$this->passengerNo = 'Passenger: '.$passID;
-		return array(
-			'passenger'	=> $this->getPassengerDetails($passID),
-			'flight'	=> $this->getFlightDetails($passID),
-			'baggage'	=> $this->getBaggageDetails($passID)
-			);
+		$this->passengerNo = 'Passenger: '.$passID;
+		$details =  array(
+				'passenger'	=> $this->getPassengerDetails($passID),
+				'flight'	=> $this->getFlightDetails($passID),
+				'baggage'	=> $this->getBaggageDetails($passID)
+				);
+				
+		return $details;		
 	} 
 
 	public function getPassengerDetails($passID){
 		$passenger = new Passenger($passID);
 		$passengerDetails = $this->passengerNo;
-		$passengerDetails .= ', '.$passenger->getPassengerName();
-		$passengerDetails .= ', '.$passenger->getPassengerAddress();
-		$passengerDetails .= ', '.$passenger->getPassengerAge();
+		$passengerDetails .= ', ' . $passenger->getPassengerName();
+		$passengerDetails .= ', ' . $passenger->getPassengerAddress();
+		$passengerDetails .= ', ' . $passenger->getPassengerAge();
+		
 		return $passengerDetails;
-	}
+	} 
 
 	public function getFlightDetails($passID){
 		$flight = new Flight($passID);
 		$flightDetails 	= $flight->getFlightNo();
-		$flightDetails .= ', '.$flight->getAirLine();
-		$flightDetails .= ', '.$flight->getDepatureTime();
+		$flightDetails .= ', ' . $flight->getAirLine();
+		$flightDetails .= ', ' . $flight->getDepatureTime();
+		
 		return $flightDetails;
 	}
 
 	public function getBaggageDetails($passID){
 		$baggage = new Baggage($passID);
 		$bagDetails = $baggage->getAmount();
-		$bagDetails .= ', '.$baggage->getTotalWeight();
-		$bagDetails .= ', '.$baggage->getAllAccountedFor();
+		$bagDetails .= ', ' . $baggage->getTotalWeight();
+		$bagDetails .= ', ' . $baggage->getAllAccountedFor();
 
 		return $bagDetails;
 	}
