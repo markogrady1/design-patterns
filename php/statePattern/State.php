7c7d67e515f38f0fb2a5151e047e427690b01039
markogrady1 <markogrady.code@gmail.com>
2014-08-05 00:41:34 +0100

    Add code conventions

--- php/statePattern/State.php
+++ php/statePattern/State.php
@@ -1,9 +1,15 @@
 <?php  namespace php\statePattern;
 
 interface State {
+	
 	public function checkAllSystems();
+	
 	public function startPowerUnits();
+	
 	public function retractArms();
+	
 	public function shutDown();
+	
 	public function rocketBooster();
+	
 }
