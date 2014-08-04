90639c71e0d0a663f9c2543b043bcdabcbd7f833
markogrady1 <markogrady.code@gmail.com>
2014-08-05 00:37:05 +0100

    Add code conventions

--- php/statePattern/FireRockets.php
+++ php/statePattern/FireRockets.php
@@ -1,31 +1,31 @@
 <?php namespace php\statePattern;
 
-class FireRockets implements State{
-public $missionControl;
-
-public function __construct(MissionControl $missionControl){
+class FireRockets implements State {
+	public $missionControl;
+	
+	public function __construct(MissionControl $missionControl) {
 		$this->missionControl = $missionControl;
 	}
 
-	public function checkAllSystems(){
+	public function checkAllSystems() {
 		echo "Systems have already been cleared.<br>";
 	}
 
-	public function startPowerUnits(){
+	public function startPowerUnits() {
 		echo "Systems have already been cleared.<br>";
 	}
 
-	public function retractArms(){
+	public function retractArms() {
 		echo "Arms and fuel connections have been retracted.<br>";
 		$this->missionControl->setState($this->missionControl->getRocketBooster());
 	}
 
-	public function shutDown(){
+	public function shutDown() {
 		echo "All systems are to cease operations immediatly.<br>";
 		$this->missionControl->liftOffHandOver($this->missionControl->getAbortProcedure());
 	}
 
-	public function rocketBooster(){
+	public function rocketBooster() {
 		echo "power units have not been started.<br>";
 	}
 }
