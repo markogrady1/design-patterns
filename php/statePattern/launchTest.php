5d4e9f97690b0cd6778bfcde39d13124af358a3e
markogrady1 <markogrady.code@gmail.com>
2014-08-05 00:44:31 +0100

    Add code writing conventions

--- php/statePattern/launchTest.php
+++ php/statePattern/launchTest.php
@@ -3,7 +3,11 @@
 include_once '../../autoload.php';
 
 $missionControl = new MissionControl(60);
- $missionControl->checkAllSystems();
- $missionControl->startPowerUnits();
- $missionControl->retractArms();
- $missionControl->rocketBooster();
+
+$missionControl->checkAllSystems();
+
+$missionControl->startPowerUnits();
+
+$missionControl->retractArms();
+
+$missionControl->rocketBooster();
