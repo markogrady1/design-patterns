88c8f1eb37579d81ea11abd3bb884819df476a59
markogrady1 <markogrady.code@gmail.com>
2014-08-04 04:30:05 +0100

    Update CookieUse.php

--- php/factoryPattern/CookieUse.php
+++ php/factoryPattern/CookieUse.php
@@ -6,7 +6,11 @@ use php\factoryPattern\CookieFactory;
 include_once('../../autoload.php');
 
 $cookie = new Cookie();
+
 $cs = new CookieStore(new CookieFactory());
+
 $cs->takeOrder('double-choc',$cookie);
+
 $cs->takeOrder('choc',$cookie);
+
 $cs->takeOrder('hazlenut',$cookie);
