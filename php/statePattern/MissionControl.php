6942d076eadf0c7850d5abf7215dfaa4dd8c1b8c
markogrady1 <markogrady.code@gmail.com>
2014-08-05 00:40:55 +0100

    Add code conventions

--- php/statePattern/MissionControl.php
+++ php/statePattern/MissionControl.php
@@ -1,74 +1,88 @@
 <?php  namespace php\statePattern;
 
-class MissionControl{
+class MissionControl {
+	
 	public $state;
+	
 	public $systemsGo;
+	
 	public $mainEngine;
+	
 	public $fireRockets;
+	
 	public $missionAbort;
+	
 	public $liftOff ;
+	
 
-	public function __construct($tMinus){
+	public function __construct($tMinus) {
 	$this->systemsGo = new SystemsGo($this);
 	$this->mainEngine = new MainEngine($this);
 	$this->fireRockets = new FireRockets($this);
 	$this->missionAbort = new MissionAbort($this);
 	$this->liftOff = new LiftOff($this);
-		if($tMinus>=60){
+	
+		if($tMinus>=60) {
 			echo 'Systems prepare for checks<br>';
 			$this->state = new SystemsGo($this);
 		}
-		if($tMinus<60){
+		
+		if($tMinus<60) {
 			echo 'Incorrect procedure. Mission abort.<br>';
 			$this->state = new MissionAbort($this);
 			$this->state->shutDown();
 		}
 	}
 
-	public function setState(State $state){
+	public function setState(State $state) {
 		$this->state = $state;
 	}
 
-	public function liftOffHandOver($orbitState){
+	public function liftOffHandOver($orbitState) {
 		echo $orbitState;
 	}
 
-	public function checkAllSystems(){
+	public function checkAllSystems() {
 		$this->state->checkAllSystems();
 	}
 
-	public function startPowerUnits(){
+	public function startPowerUnits() {
 		$this->state->startPowerUnits();
 	}
 
-	public function retractArms(){
+	public function retractArms() {
 		$this->state->retractArms();
 	}
 
-	public function rocketBooster(){
+	public function rocketBooster() {
 		$this->state->rocketBooster();
 	}
-	public function shutDown(){
+	public function shutDown() {
 		$this->state->shutDown();
 	}
 
-	public function getStartPowerUnits(){
+	public function getStartPowerUnits() {
+		
 		return $this->mainEngine;
 	}
 	
-	public function getRetractArms(){
+	public function getRetractArms() {
+		
 		return $this->fireRockets;
 	}
 
-	public function getRocketBooster(){
+	public function getRocketBooster() {
+		
 		return $this->liftOff;
 	}
 	
-	public function getAbortProcedure(){
+	public function getAbortProcedure() {
+		
 		return "All systems are shutting down and fuel is being drained.<br>";
 	}
 	
-	public function getLiftOffStatus(){
+	public function getLiftOffStatus() {
+		
 		return "We have a successful lift-Off.<br>";
 	}
 
