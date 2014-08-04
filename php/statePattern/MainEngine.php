4283b7c6e73698b5af5b068007dfbaef14f57d6b
markogrady1 <markogrady.code@gmail.com>
2014-08-05 00:38:23 +0100

    Add code conventions

--- php/statePattern/MainEngine.php
+++ php/statePattern/MainEngine.php
@@ -1,31 +1,31 @@
 <?php  namespace php\statePattern;
 
-class MainEngine implements State{
-public $missionControl;
-
-public function __construct(MissionControl $missionControl){
+class MainEngine implements State {
+	public $missionControl;
+	
+	public function __construct(MissionControl $missionControl) {
 		$this->missionControl = $missionControl;
 	}
 
-	public function checkAllSystems(){
+	public function checkAllSystems() {
 		echo "Systems have already been cleared.<br>";
 	}
 	
-	public function startPowerUnits(){
+	public function startPowerUnits() {
 		echo "power units started.<br>";
 		$this->missionControl->setState($this->missionControl->getRetractArms());
 	}
 	
-	public function retractArms(){
+	public function retractArms() {
 		echo "Power units have not been started.<br>";
 	}
 	
-	public function shutDown(){
+	public function shutDown() {
 		echo "All systems are to cease operations immediatly.<br>";
 		$this->missionControl->liftOffHandOver($this->missionControl->getAbortProcedure());
 	}
 	
-	public function rocketBooster(){
+	public function rocketBooster() {
 		echo "power units have not been started.<br>";
 	}
 }
