436ab9b671cc1714786333d9d0292a14b9947705
markogrady1 <markogrady.code@gmail.com>
2014-08-05 00:23:43 +0100

    Add conventions to code

--- php/proxyPattern/proxyUse.php
+++ php/proxyPattern/proxyUse.php
@@ -1,9 +1,11 @@
-<?php 
-use php\proxyPattern\ProxyLogo;
+<?php use php\proxyPattern\ProxyLogo;
 
 include_once '../../autoload.php';
 
 $logo1 = new ProxyLogo('shopLogo.png');
+
 $logo2 = new ProxyLogo('webLogo.png');
+
 $logo1->displayLogo();
+
 $logo2->displayLogo();
