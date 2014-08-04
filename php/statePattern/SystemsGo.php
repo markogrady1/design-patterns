cdb12bd8ada387e7525569eb7f3639ad0787a7dc
markogrady1 <markogrady.code@gmail.com>
2014-08-05 00:42:19 +0100

    Add code conventions

--- php/statePattern/SystemsGo.php
+++ php/statePattern/SystemsGo.php
@@ -1,31 +1,32 @@
 <?php  namespace php\statePattern;
 
-class SystemsGo implements State{
+class SystemsGo implements State {
+	
 	public $missionControl;
 
-	public function __construct(MissionControl $missionControl){
+	public function __construct(MissionControl $missionControl) {
 		$this->missionControl = $missionControl;
 	}
 
-	public function checkAllSystems(){
+	public function checkAllSystems() {
 		echo "All system are ready<br>";
 		$this->missionControl->setState($this->missionControl->getStartPowerUnits());
 	}
 	
-	public function startPowerUnits(){
+	public function startPowerUnits() {
 		echo "systems are not ready yet";
 	}
 	
-	public function retractArms(){
+	public function retractArms() {
 		echo "systems are not ready yet";
 	}
 	
-	public function shutDown(){
+	public function shutDown() {
 		echo "All systems are to cease operations immediatly.<br>";
 		$this->missionControl->liftOffHandOver($this->missionControl->getAbortProcedure());
 	}
 	
-	public function rocketBooster(){
+	public function rocketBooster() {
 		echo "systems are not ready yet";
 	}
 }
