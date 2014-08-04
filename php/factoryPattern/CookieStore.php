8bf684c85accff685f4fd50d13eb9185d7a0e66d
markogrady1 <markogrady.code@gmail.com>
2014-08-04 04:29:39 +0100

    Update CookieStore.php

--- php/factoryPattern/CookieStore.php
+++ php/factoryPattern/CookieStore.php
@@ -13,15 +13,16 @@ class CookieStore{
 	private $factory = null;
 	private $cookie = null;
 	public function __construct(CookieFactory $factory){
-		$this->factory=$factory;
+		$this->factory = $factory;
 	}
 	
 	public function takeOrder($type, Cookie $cookie){
-		$this->cookie= $cookie;
-		$this->cookie= $this->factory->createCookie($type);
+		$this->cookie = $cookie;
+		$this->cookie = $this->factory->createCookie($type);
 		$this->cookie->mix();
 		$this->cookie->bake();
 		$this->cookie->box();
+		
 		return $this->cookie;
 	}
 }
