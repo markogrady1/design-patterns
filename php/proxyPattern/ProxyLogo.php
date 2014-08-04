8a505c02b8fadc328e9430ea48339585fc14eb73
markogrady1 <markogrady.code@gmail.com>
2014-08-05 00:21:37 +0100

    Update ProxyLogo.php

--- php/proxyPattern/ProxyLogo.php
+++ php/proxyPattern/ProxyLogo.php
@@ -1,18 +1,19 @@
 <?php namespace php\proxyPattern;
 
 
-class ProxyLogo implements Logo{
+class ProxyLogo implements Logo {
+	
 	private $logo;
 	private $display;
 
-	public function __construct($logo){
+	public function __construct($logo) {
 		$this->logo = $logo;
 	}
 
-	public function displayLogo(){
+	public function displayLogo() {
 		if(is_null($this->display))
 			$this->display = new RealLogo($this->logo);
+			
 		return $this->display->displayLogo();
 	}
-
 }
