10ae0ae17c78566dee03aad71519f67e7d1e2692
markogrady1 <markogrady.code@gmail.com>
2014-08-07 04:25:09 +0100

    Add coding style

--- php/strategyPattern/GameSim.php
+++ php/strategyPattern/GameSim.php
@@ -5,17 +5,33 @@ use php\strategyPattern\NoWeapon;
 use php\strategyPattern\Gun;
 use php\strategyPattern\Prince;
 use php\strategyPattern\Sword;
+
+
 include_once('../../autoload.php');
 
+
+
 $king = new King;
+
 $king->shout();
+
 $king->setWeapon(new Axe);
+
 $king->useWeapon();
+
 $king->setWeapon(new NoWeapon);
+
 $king->useWeapon();
+
 $king->setWeapon(new Gun);
+
 $king->useWeapon();
+
 $king->shout();
+
 $prince = new Prince();
+
 $prince->shout();
+
 $prince->setWeapon(new Sword);
+
