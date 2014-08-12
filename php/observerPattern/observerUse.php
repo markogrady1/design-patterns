38367ffec8869886bbbb867761038f2464ab0fbe
markogrady1 <markogrady.code@gmail.com>
2014-08-13 00:36:20 +0100

    Clean up code

--- php/observerPattern/observerUse.php
+++ php/observerPattern/observerUse.php
@@ -8,11 +8,17 @@ include_once('../../autoload.php');
 $adminDesk = new AdminDesk();
 
 $magUser = new MagazineUser();
+
 $magUser->userReg($adminDesk);
+
 $appUser = new ApplicationUser();
+
 $appUser->userReg($adminDesk);
 
-$adminDesk->setConferenceDetails('London',1000,'Prince Albert Hall');
-$adminDesk->setConferenceDetails('New York',20000,'New Jersey Hall');
+$adminDesk->setConferenceDetails('London', 1000, 'Prince Albert Hall');
+
+$adminDesk->setConferenceDetails('New York', 20000, 'New Jersey Hall');
+
 $adminDesk->displayDetails();
+
 $adminDesk->removeUser($magUser);
