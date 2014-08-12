20721bea0c6bddb3326e9bd90004968433eba06e
markogrady1 <markogrady.code@gmail.com>
2014-08-13 00:33:18 +0100

    Clean up code

--- php/observerPattern/Administrator.php
+++ php/observerPattern/Administrator.php
@@ -9,7 +9,7 @@
 |===========================================================
 |
 */
-interface Administrator{
+interface Administrator {
 	public function registerUser(User $user);
 	public function removeUser(User $user);
 	public function notifyUser();
