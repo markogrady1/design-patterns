15a906e79b35e1762d4911ea4f3404013c5ba1b4
markogrady1 <markogrady.code@gmail.com>
2014-08-13 00:45:03 +0100

    Clean up code

--- php/factoryPattern/DoubleChoc.php
+++ php/factoryPattern/DoubleChoc.php
@@ -1,8 +1,8 @@
 <?php  namespace php\factoryPattern;
 
-class DoubleChoc extends Cookie{
+class DoubleChoc extends Cookie {
 	
-	public function __construct(){
+	public function __construct() {
 		$this->topping = 'Choc-Chunks';
 		$this->flavour = 'Rich Choccy';
 		$this->name = 'double choc';
