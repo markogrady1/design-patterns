b362d57207f2680a88ef876806b20898453ec6e7
markogrady1 <markogrady.code@gmail.com>
2014-08-13 00:57:25 +0100

    Clean up code

--- php/facadePattern/Statistics.php
+++ php/facadePattern/Statistics.php
@@ -1,14 +1,14 @@
 <?php namespace php\facadePattern;
 
-class Statistics{
+class Statistics {
 
 	private $facade;
 
-	public function __construct(DataFacade $facade){ 
+	public function __construct(DataFacade $facade) { 
 		$this->facade = $facade;
 	}
 
-	public function retrieveData($passID){
+	public function retrieveData($passID) {
 		$clientData = new ClientData();
 		$allData = $this->facade->retrieveStats($passID);
 		$clientData->setPassenger($allData['passenger']);
