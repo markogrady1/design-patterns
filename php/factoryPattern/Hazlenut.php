a750bf89c637b0f4f4e27f44bea8150e2f032399
markogrady1 <markogrady.code@gmail.com>
2014-08-13 00:45:24 +0100

    Clean up code

--- php/factoryPattern/Hazlenut.php
+++ php/factoryPattern/Hazlenut.php
@@ -1,8 +1,8 @@
 <?php  namespace php\factoryPattern;
 
-class Hazlenut extends Cookie{
+class Hazlenut extends Cookie {
 	
-	public function __construct(){
+	public function __construct() {
 		$this->topping = 'hazlenut-bits';
 		$this->flavour = 'nutty';
 		$this->name = 'hazlenut';
