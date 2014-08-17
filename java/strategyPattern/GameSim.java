7f04f7b2658319af9f35278e3c81d7693f18f8f3
markogrady1 <markogrady.code@gmail.com>
2014-08-17 01:07:15 +0100

    Update GameSim.java

--- java/strategyPattern/GameSim.java
+++ java/strategyPattern/GameSim.java
@@ -4,14 +4,24 @@ public class GameSim {
 
 	public static void main(String[] args) {
 		King king = new King();
+		
 		king.setWeapon(new Axe());
+		
 		king.useWeapon();
+		
 		king.shout();
+		
 		Prince prince = new Prince();
+		
 		prince.setWeapon(new NoWeapon());
+		
 		prince.useWeapon();
+		
 		prince.setWeapon(new Bazooka());
+		
 		prince.useWeapon();
+		
 		prince.shout();
+		
 	}
 }
