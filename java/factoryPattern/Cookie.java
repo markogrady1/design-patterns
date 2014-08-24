15a349cac6944fdc7e97dfc72a05725736838949
markogrady1 <markogrady.code@gmail.com>
2014-08-24 19:35:33 +0100

    Update Cookie.java

--- java/factoryPattern/Cookie.java
+++ java/factoryPattern/Cookie.java
@@ -9,30 +9,23 @@ package factoryPattern;
 |===========================================================
 |
 */
-public class Cookie 
-{
+public class Cookie {
 	protected String flavour;
-	
 	protected String name;
-	
 	protected String topping;
-
 	public Cookie() {
 
 	}
 
 	public void mix() {
-		
 		System.out.println("Mixing in the " + flavour);
 	}
 
 	public void bake() {
-		
 		System.out.println("baking with " + topping + " on top");
 	}
 
 	public void box() {
-		
 		System.out.println("Your " + name + " is ready now.");
 	}
 }
