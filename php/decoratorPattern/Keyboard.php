2d4c86340aa93db54f43d2585681a3cca7b35b00
markogrady1 <markogrady.code@gmail.com>
2014-09-07 11:23:54 +0100

    Add docblocks to methods

--- php/decoratorPattern/Keyboard.php
+++ php/decoratorPattern/Keyboard.php
@@ -6,13 +6,23 @@ class Keyboard extends HardwareDecorator{
 	public function __construct(Hardware $hardware){
 		$this->hardware = $hardware;
 	}
-
+	
+	/**
+	* Get decription of hardware
+	* 
+	* @return string
+	*/
 	public function getDescription(){
 		$this->description .= $this->hardware->getDescription().' + keyboard';
 	
 		return $this->description;
 	}
-
+	
+	/**
+	* Get cost of hardware
+	* 
+	* @return string
+	*/
 	public function cost(){
 		
 		return $this->hardware->cost()+60;
