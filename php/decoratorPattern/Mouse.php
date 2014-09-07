82023e71e503bd5ab663613180bf6c56985a983a
markogrady1 <markogrady.code@gmail.com>
2014-09-07 11:32:47 +0100

    Add docblock to class

--- php/decoratorPattern/Mouse.php
+++ php/decoratorPattern/Mouse.php
@@ -1,18 +1,39 @@
 <?php  namespace php\decoratorPattern;
 
 class Mouse extends HardwareDecorator{
+	
+	/**
+	* Hardware instance
+	* 
+	* @var $hardware
+	*/
 	protected $hardware;
-
+	
+	/**
+	* Acquire Hardware dependency
+	*
+	* @param Hardware $hardware
+	*/
 	public function __construct(Hardware $hardware){
 		$this->hardware = $hardware;
 	}
-
+	
+	/**
+	* Get the description of mouse
+	* 
+	* @return string
+	*/
 	public function getDescription(){
 		$this->description .= $this->hardware->getDescription() . ' + Mouse';
 		
 		return $this->description;
 	}
-
+	
+	/**
+	* Get cost of mouse
+	* 
+	* @return int
+	*/
 	public function cost(){
 		
 		return $this->hardware->cost() + 40;
