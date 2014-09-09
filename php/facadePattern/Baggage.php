03a099fa911234004caded9b1bb64e78e863d6b6
Mark O Grady <markogrady.code@gmail.com>
2014-09-09 10:17:46 +0100

    Add docblock to class

--- php/facadePattern/Baggage.php
+++ php/facadePattern/Baggage.php
@@ -8,25 +8,69 @@
 |========================================================================
 */
 class Baggage {
+	
+/**
+ * Total weight of baggage
+ * 
+ * @var string $totalWeight
+ */
   private $totalWeight = '5KG';
+  
+/**
+ * Default baggage ammount
+ * 
+ * @var string $ammount
+ */
   private $amount = '3';
+  
+/**
+ * Total weight of baggage
+ * 
+ * @var string $accountedFor
+ */
   private $allAccountedFor = 'true';
+  
+/**
+ * Baggage ID
+ * 
+ * @var string $id
+ */
   private $id;
-
+	
+	/**
+	 * Obtain ID when object is created
+	 * 
+	 * @param string $passID
+	 * @return void
+	 */
   	public function __construct($passID) {
 		$this->id = $passID;
   	}  
-
+       /**
+	* Get total weight of baggage
+	* 
+	* @return string
+	*/ 
   	public function getTotalWeight() {
   		
   		return $this->totalWeight;
   	}
 
+       /**
+	* Get amount baggage ammout
+	* 
+	* @return string
+	*/ 
 	public function getAmount() {
 		
   		return $this->amount;
   	}
 
+       /**
+	* Query all bags are accounted for
+	* 
+	* @return string
+	*/ 
 	public function getAllAccountedFor() {
 		
   		return $this->allAccountedFor;
