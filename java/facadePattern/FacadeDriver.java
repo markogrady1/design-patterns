3f9956f8c9c898faf55e195828c8f59a65eaf3e8
Mark O Grady <markogrady.code@gmail.com>
2014-09-25 15:50:29 +0100

    Inject ClientData class into Statistics class

--- java/facadePattern/FacadeDriver.java
+++ java/facadePattern/FacadeDriver.java
@@ -2,7 +2,7 @@ package facadePattern;
 
 public class FacadeDriver {
 	public static void main (String[] args){
-		Statistics statistics = new Statistics(new DataFacade());
+		Statistics statistics = new Statistics(new DataFacade(), new ClientData());
 		statistics.getData("WX4983489");
 	}
 }
