b335f0c38901994cc53dbe720003ec0dcfb91314
Mark O Grady <markogrady.code@gmail.com>
2014-09-29 14:45:22 +0100

    Add docblock to class

--- php/singletonPattern/VoucherCheck.php
+++ php/singletonPattern/VoucherCheck.php
@@ -4,7 +4,12 @@ include 'VoucherProvider.php';
 class VoucherCheck{
 
 	public function __construct() {}
-
+	
+	/**
+	 * Supply voucher 
+	 * 
+	 * @return void
+	 */
 	public function signIn() {
 		
 		$newVoucher = VoucherProvider::getVoucher();
