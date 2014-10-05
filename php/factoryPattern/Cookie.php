fbb71ddec4b1436057c486d57e91d33af2178f73
phpjump <system.got@gmail.com>
2014-10-05 12:22:57 +0100

    Add docblocks to Cookie.php

--- php/factoryPattern/Cookie.php
+++ php/factoryPattern/Cookie.php
@@ -10,17 +10,51 @@
 |
 */
 class Cookie {
-
-protected $name;
-protected $topping;
-protected $flavour;
-
+	
+	/**
+	 * Name of cookie
+	 * 
+	 * @var string $name
+	 */
+	protected $name;
+		
+	/**
+	 * Name of cookie topping
+	 * 
+	 * @var string $topping
+	 */
+	protected $topping;
+		
+	/**
+	 * Name of flavour
+	 * 
+	 * @var string $flavour
+	 */
+	protected $flavour;
+	
+	/**
+	 * Display name of flavour
+	 * 
+	 * @return void
+	 */
 	public function mix() {
 		echo 'mix in the ' . $this->flavour . '<br>';
 	}
+		
+	/**
+	 * Display name of topping
+	 * 
+	 * @return void
+	 */
 	public function bake() {
 		echo 'Baking and sprinkle ' . $this->topping . ' on top<br>';
 	}
+		
+	/**
+	 * Display name of cookie
+	 * 
+	 * @return void
+	 */
 	public function box() {
 		echo 'Your ' . $this->name . ' cookie is ready<br>';
 	}
