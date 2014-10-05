d79d13695531dbadde435568ab31e2b937230a1e
phpjump <system.got@gmail.com>
2014-10-05 12:30:08 +0100

    Add docblocks to CookieFactory class

--- php/factoryPattern/CookieFactory.php
+++ php/factoryPattern/CookieFactory.php
@@ -10,9 +10,19 @@
 |
 */
 class CookieFactory {
-
+	
+	/**
+	 * Cookie instance
+	 * 
+	 * @var Cookie $cookie
+	 */
 	private $cookie;
 
+	/**
+	 * Create new instance of a type of cookie
+	 * 
+	 * @return Cookie | DoubleChoc | Hazelnut $cookie
+	 */
 	public function createCookie($type) {
 		$this->cookie = new Cookie;
 		if($type=='choc') {
