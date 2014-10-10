dfa5bf8fde7628fed5e7f292429c63930faf4313
Mark O Grady <markogrady.code@gmail.com>
2014-10-11 00:54:11 +0100

    Update Administrator.php

--- php/observerPattern/Administrator.php
+++ php/observerPattern/Administrator.php
@@ -10,7 +10,28 @@
 |
 */
 interface Administrator {
+		
+	/**
+	 * Register user for conference
+	 * 
+	 * @param User $user
+	 * @return void
+	 */
 	public function registerUser(User $user);
+		
+	/**
+	 * Remove user from conference list
+	 * 
+	 * @param User $user
+	 * @return void
+	 */
 	public function removeUser(User $user);
+		
+	/**
+	 * Notify the user of any changes 
+	 * 
+	 * @param User $user
+	 * @return void
+	 */
 	public function notifyUser();
 }
