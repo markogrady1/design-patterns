6df9c2b2b53d0738187c49670d9e724f2dc15d0d
Mark O Grady <markogrady.code@gmail.com>
2014-10-12 14:36:34 +0100

    Add some docblocks

--- php/observerPattern/single_file/observerSingleFile.php
+++ php/observerPattern/single_file/observerSingleFile.php
@@ -52,12 +52,24 @@ class AdminDesk implements Administrator {
 	 * @var array $observer
 	 */
 	private $observer = array();
-
+	
+	/**
+	 * Register user for conference
+	 * 
+	 * @param User $user
+	 * @return void
+	 */
 	public function registerUser(User $user) {
 		$this->observer[] = $user;
 		
 	}
-
+	
+	/**
+	 * Remove user from the conference list
+	 * 
+	 * @param User $user
+	 * @return void
+	 */
 	public function removeUser(User $user) {
 		foreach ($this->observer as $obs => $val) {
 			if($val === $user) {
@@ -66,24 +78,47 @@ class AdminDesk implements Administrator {
 		}	
 	}
 	
+	/**
+	 * Notify user of any changes to conference
+	 *
+	 * @return void
+	 */	
 	public function notifyUser() {
 
 		foreach($this->observer as $observer) {
 			$observer->update($this->city, $this->price, $this->venue);
 		}
 	}
-
+	
+	/**
+	 * Send meeting details to conference member
+	 * 
+	 * @return void
+	 */
 	public function sendConferenceDetails() {
 		$this->notifyUser();
 	}
-
+	
+	/**
+	 * Finalize conference location and meeting time
+	 * 
+	 * @param string $conferenceCity
+	 * @param string $conferencePrice
+	 * @param string $conferenceVenue
+	 * @return void
+	 */
 	public function setConferenceDetails($conferenceCity, $conferencePrice, $conferenceVenue) {
 		$this->city = $conferenceCity;
 		$this->price = $conferencePrice;
 		$this->venue = $conferenceVenue;
 		$this->sendConferenceDetails();
 	}
-
+	
+	/**
+	 * Display finalized conference details
+	 * 
+	 * @return void
+	 */
 	public function displayDetails() {
 		echo "New conference details:<br>City Location: ".$this->city."<br>Conference price: ".$this->price."<br>Conference venue: ".$this->venue;
 	}
