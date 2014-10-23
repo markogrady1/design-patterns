4509bda95daf8ee9309a9f483441e4e42bb90e8d
Mark O Grady <markogrady.code@gmail.com>
2014-10-23 11:28:28 +0100

    Update docblocks

--- php/proxyPattern/ProxyLogo.php
+++ php/proxyPattern/ProxyLogo.php
@@ -3,13 +3,29 @@
 
 class ProxyLogo implements Logo {
 	
+	/**
+	 * Logo instance
+	 * 
+	 * @var Logo $logo
+	 */
 	private $logo;
+		
+	/**
+	 * RealLogo instance
+	 * 
+	 * @var Logo $display
+	 */
 	private $display;
 
 	public function __construct($logo) {
 		$this->logo = $logo;
 	}
-
+	
+	/**
+	* Display the logo
+	*
+	* @return Logo $display
+	*/
 	public function displayLogo() {
 		if(is_null($this->display))
 			$this->display = new RealLogo($this->logo);
