9a1050cc149bc07f562d7e83b5a2de550ef71510
Mark O Grady <markogrady.code@gmail.com>
2014-11-01 12:44:06 +0000

    Update MainEngine.php

--- php/statePattern/MainEngine.php
+++ php/statePattern/MainEngine.php
@@ -1,16 +1,38 @@
 <?php  namespace php\statePattern;
 
 class MainEngine implements State {
-	public $missionControl;
 	
+	/**
+	 * Mission control instance
+	 *
+	 * @var MissionControl $missionControl
+	 */
+	public $missionControl;
+		
+	/**
+	 * Set instance for mission control
+	 *
+	 * @param MissionControl $missionControl
+	 * @return void
+	 */
 	public function __construct(MissionControl $missionControl) {
 		$this->missionControl = $missionControl;
 	}
-
+	
+	/**
+	 * Check all systems are stable and ready to go
+	 *
+	 * @return void
+	 */
 	public function checkAllSystems() {
 		echo "Systems have already been cleared.<br>";
 	}
 	
+	/**
+	 * set state in mission control
+	 *
+	 * @return void
+	 */
 	public function startPowerUnits() {
 		echo "power units started.<br>";
 		$this->missionControl->setState($this->missionControl->getRetractArms());
