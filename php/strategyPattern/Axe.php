4033ea54ffb2c12672d4432d111bf2d4f0a4b241
Mark O Grady <markogrady.code@gmail.com>
2014-11-02 18:25:07 +0000

    Add docblocks to Axe class

--- php/strategyPattern/Axe.php
+++ php/strategyPattern/Axe.php
@@ -1,7 +1,11 @@
 <?php namespace php\strategyPattern;
 
 class Axe implements Weapon {
-
+	/**
+	 * Action for the Axe weapon
+	 * 
+	 * @return void
+	 */
 	public function action() {
 		echo 'Swing Swing<br>';
 	}
