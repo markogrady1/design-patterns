c9a5fd19ee7bd33b7397f2577ced081f09a7deba
Mark O Grady <markogrady.code@gmail.com>
2014-11-03 16:06:36 +0000

    Update RealLogo.php

--- php/proxyPattern/RealLogo.php
+++ php/proxyPattern/RealLogo.php
@@ -1,14 +1,23 @@
 <?php namespace php\proxyPattern;
 
 class RealLogo implements Logo {
-	
+	/**
+	 * Logo instance 
+	 * 
+	 * @var $logo
+	 */
 	private $logo;
 
 	public function __construct($logo) {
 		$this->logo = $logo;
 		echo "Logo loading:............<br> ";
 	}
-
+	
+	/**
+	 * Logo display
+	 * 
+	 * @return void
+	 */
 	public function displayLogo() {
 		printf("Logo: %s <br>",$this->logo);
 	}
